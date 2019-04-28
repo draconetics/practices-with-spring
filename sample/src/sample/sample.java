07203ed2d71ae2b85fa0d09ddb9ef66943435362
mario flores <marioflorescondori@gmail.com>
2019-04-28 17:14:40 -0400

    starting the project

---
+++ sample/src/sample/sample.java
@@ -0,0 +1,12 @@
+package sample;
+
+public class sample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//just printing to probe that everything is ok.
+		System.out.println("hello world..");
+
+	}
+
+}
