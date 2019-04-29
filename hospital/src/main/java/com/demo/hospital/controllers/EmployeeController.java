2486c403fc5bc46825364bb04b44a495339c4a2b
mario flores <marioflorescondori@gmail.com>
2019-04-29 12:03:30 -0400

    swagger and minisample

---
+++ hospital/src/main/java/com/demo/hospital/controllers/EmployeeController.java
@@ -0,0 +1,94 @@
+/**
+ * @author: Edson A. Terceros T.
+ */
+
+package com.demo.hospital.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RequestMapping(value = "/users")
+@RestController
+public class EmployeeController {
+
+	
+	 @RequestMapping(
+	            value = "/",
+	            method = RequestMethod.GET
+    )
+	public String findAsus() {
+		 return "hello world";
+	}
+	 
+	
+
+	/*
+    public EmployeeController(EmployeeService service) {
+        this.service = service;
+    }
+
+    @GET
+    public Response getEmployees() {
+        List<EmployeeCommand> employeeList = new ArrayList<>();
+        service.findAll().forEach(employee -> {
+            employeeList.add(new EmployeeCommand(employee));
+        });
+        return Response.ok(employeeList).build();
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getEmployeeById(@PathParam("id") long id) {
+        Employee employee = service.findById(id);
+        return Response.ok(new EmployeeCommand(employee)).build();
+    }
+
+    @OPTIONS
+    public Response prefligth() {
+        return Response.ok().build();
+    }
+
+    @POST
+    public EmployeeCommand addEmployee(EmployeeCommand employeeCommand) {
+        Employee employee = service.save(employeeCommand.toEmployee());
+        return new EmployeeCommand(employee);
+    }
+
+    @PUT
+    public EmployeeCommand updateEmployee(EmployeeCommand employeeCommand) {
+        Employee e = service.findById(employeeCommand.getId());
+        e.setFirstName(employeeCommand.getFirstName());
+        e.setLastName(employeeCommand.getLastName());
+        service.save(e);
+        return new EmployeeCommand(e);
+//        Employee employee = service.save(employeeCommand.toEmployee());
+//        return new EmployeeCommand(employee);
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public void deleteEmployee(@PathParam("id") long id) {
+        service.deleteById(id);
+    }
+
+    @Path("/{id}/image")
+    @POST
+    @Consumes(MediaType.MULTIPART_FORM_DATA)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response uploadFile(@PathParam("id") String id,
+                               @FormDataParam("file") InputStream file,
+                               @FormDataParam("file") FormDataContentDisposition fileDisposition) {
+        service.saveImage(Long.valueOf(id), file);
+        return Response.ok("Data uploaded successfully !!").build();
+    }
+
+    /*
+    https://www.getpostman.com/collections/cb9764af6c5d5bcaa0c9
+    */
+}
