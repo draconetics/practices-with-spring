58e0175479bcf2b415c41ec064f1e3935a3c440d
mario flores <marioflorescondori@gmail.com>
2019-05-02 10:18:47 -0400

    deploying restfull application

--- hospital/src/main/java/com/demo/hospital/HospitalApplication.java
+++ hospital/src/main/java/com/demo/hospital/HospitalApplication.java
@@ -2,13 +2,20 @@ package com.demo.hospital;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class HospitalApplication {
+public class HospitalApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(HospitalApplication.class, args);
 		System.out.println("Hospital system");
 	}
-
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(HospitalApplication.class);
+	}
 }
